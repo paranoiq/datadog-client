4d38bf2edf9547f9f48d6fb26470e79a58aa1cc5
Vlasta Neubauer <paranoiq@centrum.cz>
2013-10-05 18:54:59 +0200

    get rid of static API

--- src/DogStatsd.php
+++ src/DogStatsd.php
@@ -1,23 +1,36 @@
 <?php
+
+namespace DataDog;
+
 /**
  * Datadog implementation of StatsD
  * Added the ability to Tag!
  *
- * Most of this code was stolen from: https://gist.github.com/1065177/5f7debc212724111f9f500733c626416f9f54ee6
- *
- * I did make it the most effecient UDP process possible, and add tagging.
- *
  * @author Alex Corley <anthroprose@gmail.com>
+ * @author Laurence Roberts <lsjroberts@gmail.com>
  **/
+class StatsdClient
+{
 
-class Datadogstatsd {
+    private $eventUrl = '/api/v1/events';
+    private $server = 'localhost';
 
-    static private $__server = 'localhost';
-    static private $__datadogHost;
-    static private $__eventUrl = '/api/v1/events';
-    static private $__apiKey;
-    static private $__applicationKey;
-    static private $__curlHandler;
+    private $datadogHost;
+    private $apiKey;
+    private $applicationKey;
+
+    private $timings = array();
+
+    /** @var resource cURL multi handler */
+    private $curl;
+
+
+    public function __construct($host, $apiKey, $appKey)
+    {
+        $this->datadogHost = $host;
+        $this->apiKey = $apiKey;
+        $this->applicationKey = $appKey;
+    }
 
     /**
      * Log timing information
@@ -26,10 +39,44 @@ class Datadogstatsd {
      * @param float $time The ellapsed time (ms) to log
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      **/
-    public static function timing($stat, $time, $sampleRate = 1, array $tags = null) {
+    public function timing($stat, $time, $sampleRate = 1, array $tags = null)
+    {
+        $this->send(array($stat => "$time|ms"), $sampleRate, $tags);
+    }
 
-        static::send(array($stat => "$time|ms"), $sampleRate, $tags);
+    /**
+     * Start a timing log
+     *
+     * @param  string  $stat       The metric to log timing info for.
+     * @param  float|1 $sampleRate The rate (0-1) for sampling
+     * @param  array  $tags        Optional tags for the metric
+     *
+     * @return void
+     */
+    public function startTiming($stat, $sampleRate = 1, array $tags = null)
+    {
+        $this->timings[$stat] = array(
+            'start' => microtime(true),
+            'sampleRate' => $sampleRate,
+            'tags' => $tags
+        );
+    }
 
+    /**
+     * End a timing log and send to statsd
+     *
+     * @param  string $stat The metric to log timing info for.
+     *
+     * @return void
+     */
+    public function endTiming($stat)
+    {
+        if (isset($this->timings[$stat])) {
+            $timing = $this->timings[$stat];
+            $this->timing($stat, microtime(true) - $timing['start'], $timing['sampleRate'], $timing['tags']);
+        } else {
+            throw new \Exception("Timing '" . $stat . "' has not been started");
+        }
     }
 
     /**
@@ -39,10 +86,9 @@ class Datadogstatsd {
      * @param float $value The value
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      **/
-    public static function gauge($stat, $value, $sampleRate = 1, array $tags = null) {
-
-        static::send(array($stat => "$value|g"), $sampleRate, $tags);
-
+    public function gauge($stat, $value, $sampleRate = 1, array $tags = null)
+    {
+        $this->send(array($stat => "$value|g"), $sampleRate, $tags);
     }
 
     /**
@@ -52,10 +98,9 @@ class Datadogstatsd {
      * @param float $value The value
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      **/
-    public static function histogram($stat, $value, $sampleRate = 1, array $tags = null) {
-
-        static::send(array($stat => "$value|h"), $sampleRate, $tags);
-
+    public function histogram($stat, $value, $sampleRate = 1, array $tags = null)
+    {
+        $this->send(array($stat => "$value|h"), $sampleRate, $tags);
     }
 
     /**
@@ -65,10 +110,9 @@ class Datadogstatsd {
      * @param float $value The value
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      **/
-    public static function set($stat, $value, $sampleRate = 1, array $tags = null) {
-
-        static::send(array($stat => "$value|s"), $sampleRate, $tags);
-
+    public function set($stat, $value, $sampleRate = 1, array $tags = null)
+    {
+        $this->send(array($stat => "$value|s"), $sampleRate, $tags);
     }
 
 
@@ -79,10 +123,9 @@ class Datadogstatsd {
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      * @return boolean
      **/
-    public static function increment($stats, $sampleRate = 1, array $tags = null) {
-
-        static::updateStats($stats, 1, $sampleRate, $tags);
-
+    public function increment($stats, $sampleRate = 1, array $tags = null)
+    {
+        $this->updateStats($stats, 1, $sampleRate, $tags);
     }
 
     /**
@@ -92,10 +135,9 @@ class Datadogstatsd {
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      * @return boolean
      **/
-    public static function decrement($stats, $sampleRate = 1, array $tags = null) {
-
-        static::updateStats($stats, -1, $sampleRate, $tags);
-
+    public function decrement($stats, $sampleRate = 1, array $tags = null)
+    {
+        $this->updateStats($stats, -1, $sampleRate, $tags);
     }
 
     /**
@@ -108,20 +150,19 @@ class Datadogstatsd {
      *
      * @return boolean
      **/
-    public static function updateStats($stats, $delta = 1, $sampleRate = 1, array $tags = null) {
-
-        if (!is_array($stats)) { $stats = array($stats); }
+    public function updateStats($stats, $delta = 1, $sampleRate = 1, array $tags = null)
+    {
+        if (!is_array($stats)) {
+            $stats = array($stats);
+        }
 
         $data = array();
 
         foreach($stats as $stat) {
-
             $data[$stat] = "$delta|c";
-
         }
 
-        static::send($data, $sampleRate, $tags);
-
+        $this->send($data, $sampleRate, $tags);
     }
 
     /**
@@ -132,67 +173,46 @@ class Datadogstatsd {
      *
      * @return null
      **/
-    public static function send($data, $sampleRate = 1, array $tags = null) {
-
+    protected function send($data, $sampleRate = 1, array $tags = null)
+    {
         // sampling
         $sampledData = array();
 
         if ($sampleRate < 1) {
-
             foreach ($data as $stat => $value) {
-
                 if ((mt_rand() / mt_getrandmax()) <= $sampleRate) {
-
                     $sampledData[$stat] = "$value|@$sampleRate";
-
                 }
-
             }
-
         } else {
-
             $sampledData = $data;
-
         }
 
-        if (empty($sampledData)) { return; }
+        if (empty($sampledData)) {
+            return;
+        }
 
         // Non - Blocking UDP I/O - Use IP Addresses!
         $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
         socket_set_nonblock($socket);
 
         foreach ($sampledData as $stat => $value) {
-
             if ($tags !== NULL && is_array($tags) && count($tags) > 0) {
-
                 $value .= '|';
 
                 foreach ($tags as $tag_key => $tag_val) {
-
                     $value .= '#' . $tag_key . ':' . $tag_val . ',';
-
                 }
 
                 $value = substr($value, 0, -1);
-
             } elseif (isset($tags) && !empty($tags)) {
-
                 $value .= '|#' . $tags;
-
             }
 
-            socket_sendto($socket, "$stat:$value", strlen("$stat:$value"), 0, static::$__server, 8125);
-
+            socket_sendto($socket, "$stat:$value", strlen("$stat:$value"), 0, $this->server, 8125);
         }
 
         socket_close($socket);
-
-    }
-
-    public static function configure($apiKey, $applicationKey, $datadogHost = 'https://app.datadoghq.com') {
-        static::$__apiKey = $apiKey;
-        static::$__applicationKey = $applicationKey;
-        static::$__datadogHost = $datadogHost;
     }
 
     /**
@@ -205,7 +225,8 @@ class Datadogstatsd {
      *   http://api.datadoghq.com/events for the valid keys
      * @return null
      **/
-    public static function event($title, $vals = array()) {
+    public function event($title, $vals = array())
+    {
         // Assemble the request
         $vals['title'] = $title;
         // Convert a comma-separated string of tags into an array
@@ -220,12 +241,12 @@ class Datadogstatsd {
         $body = json_encode($vals); // Added in PHP 5.3.0
 
         // Get the url to POST to
-        $url = static::$__datadogHost . static::$__eventUrl
-             . '?api_key='            . static::$__apiKey
-             . '&application_key='    . static::$__applicationKey;
+        $url = $this->datadogHost . $this->eventUrl
+            . '?api_key='          . $this->apiKey
+            . '&application_key='  . $this->applicationKey;
 
         // Set up the http request. Need the PECL pecl_http extension
-        static::sendEvent($url, $body);
+        $this->sendEvent($url, $body);
     }
 
     /**
@@ -235,10 +256,10 @@ class Datadogstatsd {
      * @param string $body
      * @return null
      */
-    public static function sendEvent($url, $body) {
+    protected function sendEvent($url, $body) {
         // create the multiple cURL handle
-        if (!self::$__curlHandler) {
-            self::$__curlHandler = curl_multi_init();
+        if (!$this->curl) {
+            $this->curl = curl_multi_init();
         }
 
         // create cURL resource
@@ -251,12 +272,12 @@ class Datadogstatsd {
         curl_setopt($request, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 
         // add request
-        curl_multi_add_handle(self::$__curlHandler,$request);
+        curl_multi_add_handle($this->curl, $request);
 
         // execute the request
         $active = null;
         do {
-            $mrc = curl_multi_exec(self::$__curlHandler, $active);
+            $mrc = curl_multi_exec($this->curl, $active);
         } while ($mrc == CURLM_CALL_MULTI_PERFORM);
 
         // do not wait for results
